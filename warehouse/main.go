bedf1b5ad66b88d7ae673f2121be0aab03be000c
Hesham Moneer <83174696+HeshamMoneer@users.noreply.github.com>
2021-09-08 20:46:50 +0200

    go9

--- warehouse/main.go
+++ warehouse/main.go
@@ -40,8 +40,7 @@ func main() {
 
 	token := os.Getenv("GH_TOKEN")
 	if token == "" {
-		//panic("GH_TOKEN environment variable is not set")
-		token = ${{ secrets.GH_TOKEN }}
+		panic("GH_TOKEN environment variable is not set")
 	}
 
 	ghURL := os.Getenv("GH_URL")
